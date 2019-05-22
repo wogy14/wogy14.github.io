85719e4ff2b63c3af6b0d0f4bc2eab79d862db54
wogy14 <wogy.wogy14@gmail.com>
2019-05-22 20:32:51 +0300

    add for business

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ function menu(){
         $(".menu_mob ul").hide("slow");
     }
 }
+$(document).ready(function(){
+    $('.feedback_slider').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        prevArrow:'<span class="slick-prev"><i class="fas fa-angle-left"></i></span>',
+        nextArrow:'<span class="slick-next"><i class="fas fa-angle-right"></i></span>',
+        responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }       
+      ]
+    });
+});
