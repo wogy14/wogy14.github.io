2ee6e12c279d93874d88d73590991eb5a00e6e24
Oleksii Havryshkiv <oleksii.havryshkiv@agiliway.com>
2020-05-17 22:44:14 +0300

    Add web_lab3. React APP.

---
+++ web_lab3/precache-manifest.10f7b7dbb64a61c1b60c90bd7b0b9c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02924ac20d3ff98c88695c3ac8066d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9300b3d1cd40bc77a2",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7c744797b536e39f265f",
+    "url": "/static/css/main.973055cc.chunk.css"
+  },
+  {
+    "revision": "eb9300b3d1cd40bc77a2",
+    "url": "/static/js/2.74b4c380.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.74b4c380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c744797b536e39f265f",
+    "url": "/static/js/main.dfcc82d6.chunk.js"
+  },
+  {
+    "revision": "09d66926d7fcfe2ee4aa",
+    "url": "/static/js/runtime-main.997a4d24.js"
+  }
+]);
